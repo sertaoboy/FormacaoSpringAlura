a48a4bb2d5a9973cfb0331ec923a8a340daf49f5
Raul Holanda <raul.dev@edu.unifor.br>
2024-10-11 15:39:38 -0300

    feature for return description about seasons

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -2,12 +2,16 @@ package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -28,11 +32,28 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		ConverteDados conversor = new ConverteDados();
 
 		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dadosSerie);
+		//System.out.println(dadosSerie);
 
 		json = consumoApi.obterDados("https://www.omdbapi.com/?t=the+witcher&season=2&episode=3&apikey="+apiKey);
 		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
+		//System.out.println(dadosEpisodio);
+
+
+		List<DadosTemporada> dadosTemporadas = new ArrayList<>();
+
+		for(int i = 1; i<=dadosSerie.totalTemporadas(); i++) {
+			json = consumoApi.obterDados("https://www.omdbapi.com/?t=the+witcher&season="+i+"&apikey="+apiKey);
+			DadosTemporada dadosTemporada = conversor.obterDados(json,DadosTemporada.class);
+			dadosTemporadas.add(dadosTemporada);
+		}
+
+		for(DadosTemporada d : dadosTemporadas) {
+			System.out.println(d);
+		}
+
+
+
+
 
 
 
