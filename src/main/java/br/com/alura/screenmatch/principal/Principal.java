e027bb5169ca74a5153bf4a216fb5eb2d4d31a71
Raul Holanda <raul.dev@edu.unifor.br>
2024-10-15 11:49:34 -0400

    filtering and returning average between each season

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -97,11 +97,11 @@ public class Principal {
                                 "Episodio:"+e.getTitulo() +
                                 "Data lancamento:"+e.getDataLancamento().format(formatter)
                 ));
-
-
-
-
-
+        Map<Integer, Double>avaliacoesPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,
+                        Collectors.averagingDouble(Episodio::getAvaliacao)));
+        System.out.println(avaliacoesPorTemporada); //retorna a media de cada temporada
 
 
 
