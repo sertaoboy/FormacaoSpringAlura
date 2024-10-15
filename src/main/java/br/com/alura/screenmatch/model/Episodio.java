7298672f586babc9c48a8e0e7fc896b83cc8c9a3
Raul Holanda <raul.dev@edu.unifor.br>
2024-10-15 07:19:00 -0300

    adding episode class

--- src/main/java/br/com/alura/screenmatch/model/Episodio.java
+++ src/main/java/br/com/alura/screenmatch/model/Episodio.java
@@ -1,4 +1,80 @@
 package br.com.alura.screenmatch.model;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
 public class Episodio {
+    private Integer temporada;
+    private String titulo;
+    private Integer numero;
+    private Double avaliacao;
+    private LocalDate dataLancamento;
+
+    public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio){
+        this.temporada = numeroTemporada;
+        this.titulo=dadosEpisodio.titulo();
+        this.numero=dadosEpisodio.numero();
+        try{
+            this.avaliacao=Double.valueOf(dadosEpisodio.avaliacao());
+            this.dataLancamento=LocalDate.parse(dadosEpisodio.dataDeLancamento());
+        }catch (NumberFormatException e) {
+            this.avaliacao = 0.0;
+        }catch (DateTimeException e) {
+            this.dataLancamento.equals(null);
+        }
+
+
+    }
+
+
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public Integer getTemporada() {
+        return temporada;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Episodio{" +
+                "temporada=" + temporada +
+                ", titulo='" + titulo + '\'' +
+                ", numero=" + numero +
+                ", avaliacao=" + avaliacao +
+                ", dataLancamento=" + dataLancamento +
+                '}';
+    }
 }
