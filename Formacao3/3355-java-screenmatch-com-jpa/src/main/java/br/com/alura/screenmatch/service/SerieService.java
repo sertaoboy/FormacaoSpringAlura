8743c30a179fe0c2b552e75f67fddeaa04faa13e
Raul H <raul.dev@hotmail.com>
2024-10-29 15:09:30 -0300

    refactoring SerieController and SerieService

--- Formacao3/3355-java-screenmatch-com-jpa/src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ Formacao3/3355-java-screenmatch-com-jpa/src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -1,6 +1,7 @@
 package br.com.alura.screenmatch.service;
 
 import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +14,18 @@ public class SerieService {
     @Autowired
     private SerieRepository repositorio;
 
+
     public List<SerieDTO> obterTodasAsSeries(){
-        return repositorio.findAll()
+        return converteDados(repositorio.findAll());
+    }
+
+    public List<SerieDTO> obterTop5Series() {
+        return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
+
+    }
+
+    private List<SerieDTO> converteDados(List<Serie> series){
+        return series
                 .stream()
                 .map(s -> new SerieDTO(s.getId(),s.getTitulo(),s.getTotalTemporadas(),s.getAvaliacao(),s.getGenero(),s.getAtores(),s.getPosterUrl(),s.getSinpose(),s.getPremio(),s.getLancamento(),s.getDuracao(),s.getVotacoes()))
                 .collect(Collectors.toList());
