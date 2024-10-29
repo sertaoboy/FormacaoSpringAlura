5c7aa3ef94054440911c5ce4bf762b9ab93306de
Raul H <raul.dev@hotmail.com>
2024-10-29 13:38:05 -0300

    adding DTO pattern for Serie

--- Formacao3/3355-java-screenmatch-com-jpa/src/main/java/br/com/alura/screenmatch/dto/SerieDTO.java
+++ Formacao3/3355-java-screenmatch-com-jpa/src/main/java/br/com/alura/screenmatch/dto/SerieDTO.java
@@ -1,4 +1,23 @@
 package br.com.alura.screenmatch.dto;
 
-public record SerieDTO() {
+import br.com.alura.screenmatch.model.Categoria;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record SerieDTO( Long id,
+                        String titulo,
+                        Integer totalTemporadas,
+                        Double avaliacao,
+                        Categoria genero,
+                        String atores,
+                        String posterUrl,
+                        String sinpose,
+                        String premio,
+                        LocalDate lancamento,
+                        LocalDate duracao,
+                        List<String> votacoes) {
 }
